6921252572586c718a54313a3fed1bd5395184eb
Steve <stephenkago10@gmail.com>
2022-11-08 16:11:51 +0300

    Create and delete components.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="navbar">
+            <h1 className="navh1">Booze Boodega</h1>
+            <div className="navlinks">
+              <NavLink to='/' >
+                  HOME
+              </NavLink>
+              <NavLink to='/whiskey' >
+                  WHISKEY
+              </NavLink>
+              <NavLink to='/vodka' >
+                  VODKA
+              </NavLink>
+              <NavLink to='/wine' >
+                  WINE
+              </NavLink>
+              <NavLink to='/gin' >
+                  GIN
+              </NavLink>
+            </div>
+        </nav>
   )
 }
 
