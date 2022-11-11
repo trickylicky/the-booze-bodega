2aec4ddb9580d4ad92033d3e572da6e4d7462bab
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-11 23:30:50 +0300

    Modify file.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,31 +4,31 @@ import { NavLink } from 'react-router-dom'
 function NavBar() {
   return (
     <nav className="navbar">
-            <h1 className="navh1">Booze Bodega</h1>
-            <div className="navlinks">
-              <NavLink to='/' >
-                  HOME
-              </NavLink>
-              <NavLink to='/whiskey' >
-                WHISKEY
-              </NavLink>
-              <NavLink to='/gin' >
-                  GIN
-              </NavLink>
-              <NavLink to='/vodka' >
-                  VODKA
-              </NavLink>
-              <NavLink to='/wine' >
-                  WINE
-              </NavLink>
-              <NavLink to='/blog' >
-                  BLOG
-              </NavLink>
-              <NavLink to='/addreview' >
-                  REVIEW
-              </NavLink>
-            </div>
-        </nav>
+        <h1 className="navh1">Booze Bodega</h1>
+        <div className="navlinks">
+          <NavLink to='/' >
+              HOME
+          </NavLink>
+          <NavLink to='/whiskey' >
+            WHISKEY
+          </NavLink>
+          <NavLink to='/gin' >
+              GIN
+          </NavLink>
+          <NavLink to='/vodka' >
+              VODKA
+          </NavLink>
+          <NavLink to='/wine' >
+              WINE
+          </NavLink>
+          <NavLink to='/blog' >
+              BLOG
+          </NavLink>
+          <NavLink to='/addreview' >
+              REVIEW
+          </NavLink>
+        </div>
+    </nav>
   )
 }
 
