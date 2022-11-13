2726e098a8db851d41998bc6020aa544b82d6813
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-13 10:23:10 +0300

    Node module.

--- src/components/Whiskey.js
+++ src/components/Whiskey.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Liquor from './Liquor'
+import LiquorCtaegory from './LiquorCategory'
 
 function Whiskey( {whiskeys}) {
   return (
     <div className='component whiskey'>
         <p className='first'>Whiskey</p>
         <p className='second'>There are {whiskeys.length} products.</p>
-        <Liquor props = {whiskeys} />
+        <LiquorCtaegory props = {whiskeys} />
     </div>
   )
 }
