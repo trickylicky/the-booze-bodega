aff6f5e78b664a57a7821b6aaafd16406e4f8f00
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-13 14:16:36 +0300

    Add/modify node modules.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Cart( savedItems) {
+    console.log(savedItems)
   return (
     <div className='component cart'>
         <p className='first'> View Saved Items</p>
