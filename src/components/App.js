cd1ae769ac9643da165eb711748bd967d1013d74
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-13 08:14:54 +0300

    Import Cart component and add route and state.

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,15 @@ import Gin from './Gin'
 import Vodka from './Vodka'
 import Wine from './Wine'
 import Search from './Search'
+import Cart from './Cart'
 
 function App() {
 
   const [liquors, setLiquors] = useState([])
   const [searchInput , setSearchInput] = useState([])
   const [reviews, setReviews] = useState([])
+  const [cart, setCart] = useState([]);
+
 
   const liquorsApi = "http://localhost:9292/liquors"
   const reviewsApi = "http://localhost:9292/reviews"
@@ -31,7 +34,23 @@ function App() {
     } 
    }, [reviews, searchInput])
   
+  const wines = liquors.filter( liquor => liquor.category === "Wines" )
+  const whiskeys = liquors.filter( liquor => liquor.category === "Whiskey" )
+  const vodkas = liquors.filter( liquor => liquor.category === "Vodka" )
+  const gins = liquors.filter( liquor => liquor.category === "Gin" )
+
   const handleSearch = e => setSearchInput(e.target.value)
+
+  // function addToCart(liquor) {
+  //   if (!cart.includes(liquor)) {
+  //     setCart([...cart, liquor])
+  //     fetch("http://localhost:9292/cart", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(liquor)
+  //     })
+  //   }
+  // }
 
   // const handleDelete = e => {
   //   fetch(reviewsApi+`/${e.target.id}`,{
@@ -43,23 +62,19 @@ function App() {
   //   })
   // }
 
-  const products =liquors
-  const wines = liquors.filter( liquor => liquor.category === "Wines" )
-  const whiskeys = liquors.filter( liquor => liquor.category === "Whiskey" )
-  const vodkas = liquors.filter( liquor => liquor.category === "Vodka" )
-  const gins = liquors.filter( liquor => liquor.category === "Gin" )
 
   return (
     <div>
       <NavBar />
       <Search handleSearch ={handleSearch}/>
       <Routes>
-        <Route exact path='/' element={<Home products={products}/> } />
+        <Route exact path='/' element={<Home products={liquors}/> } />
         <Route path='/whiskey' element={<Whiskey whiskeys = {whiskeys} />} />
         <Route path='/gin' element={<Gin gins= {gins} />} />
         <Route path='/vodka' element={<Vodka vodkas = {vodkas} />} />
         <Route path='/wine' element={<Wine wines = {wines} />} />
         <Route path='/blog' element={<Blog />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
       <Footer />
     </div>
