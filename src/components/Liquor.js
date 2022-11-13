0bc9bafea6fe21743074025f6c6032ed1791e05e
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-13 09:28:15 +0300

    Modify file.

--- src/components/Liquor.js
+++ src/components/Liquor.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Popup from 'reactjs-popup'
 
-function LiquorCategory({props}) {
+function LiquorCategory({props, reviews}) {
 
   return (
     <div className='component liquors'>
@@ -11,19 +11,18 @@ function LiquorCategory({props}) {
             <main id='liquor' key={obj.id}>
                 <img src={obj.image_url} alt={obj.title} />
                 <p className='liquor_details'> 
-                  <b>{obj.title}</b><br /><br />
-                  {obj.description} <br /><br />
-                  <span style={{color:'red', fontSize: 'large'}}>KSh. {obj.price}</span> <br /> <br />
-                  <Popup trigger={<button className='popup'> view</button>} position="center center">
-                    <img src={obj.image_url} alt={obj.title} />
-                    <p> 
-                      <b>{obj.title}</b><br /><br />
-                      {obj.description} <br /><br />
-                      <span style={{color:'red', fontSize: 'large'}}>KSh. {obj.price}</span> <br /> <br />
-                    </p>
-                  </Popup> &emsp; &emsp; &emsp; &emsp;
-                  <button className='popup'>add to cart</button>
-                </p>
+                  <b>{obj.title}</b>
+                </p> <br /> &emsp;
+                <Popup trigger={<button className='popup'> more details</button>} position="center center">
+                  <img src={obj.image_url} alt={obj.title} />
+                  <p> 
+                    <b>{obj.title}</b><br /><br />
+                    {obj.description} <br /><br />
+                    <span style={{color:'red', fontSize: 'large'}}>KSh. {obj.price}</span> <br /> <br />
+                  </p>
+                  <p>reviews</p>
+                </Popup> &emsp; &emsp; &emsp; &emsp;
+                <button className='popup'>add to cart</button>
             </main>
             )
           ) 
