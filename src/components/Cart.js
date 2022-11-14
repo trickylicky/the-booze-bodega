0256f21a2f9a5dd6881308292760719f6850cf5d
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-14 06:17:05 +0300

    Import component and modify file.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../services/app-context'
+import LiquorCategory from './LiquorCategory'
 
 function Cart() {
-  const { savedItems } = useContext(AppContext)
+  const { cart, addToCart } = useContext(AppContext)
 
-  console.log(savedItems)
+  console.log(cart);
+
   return (
     <div className='component cart'>
         <p className='first'> View Saved Items</p>
+        <LiquorCategory props={cart} addToCart={addToCart} />
     </div>
   )
 }
